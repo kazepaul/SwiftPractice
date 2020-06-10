5ab501519922c7a6fcbfabd396da502c177462b0
Paul Chan <paul.chan@rakuten.com>
2020-06-10 20:47:41 +0900

    Initial Commit

---
+++ SwiftPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPractice
+//
+//  Created by Chan, Paul | Paul | PAYSD on 2020/06/10.
+//  Copyright © 2020 Chan Ka Ho, Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
